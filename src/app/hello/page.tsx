3ee95b66c13e37a4d12ce50b9b5878a08e34170e
Park Hyunwoo <hyunwoo.park@izex.co.kr>
2025-02-27 09:40:50 +0900

    hello workd

---
+++ src/app/hello/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+    return (
+        <div className='pl-16 pt-16'>Hello World</div>
+    )
+}
+
+export default page
